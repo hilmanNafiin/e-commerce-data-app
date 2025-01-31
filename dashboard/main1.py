d23b88fd552d1fdc78241438c8a36f1fa118dce7
Hilman <118121948+hilmanNafiin@users.noreply.github.com>
2025-02-01 00:46:33 +0700

    Update main1.py

--- dashboard/main1.py
+++ dashboard/main1.py
@@ -27,7 +27,7 @@ max_date = all_df["order_approved_at"].max()
 # Sidebar
 with st.sidebar:
     st.title("Tugas Pengantar Sains Data")
-    st.image("anggota.png")
+    st.image("./anggota.png")
     start_date, end_date = st.date_input(
         label="Select Date Range",
         value=[min_date, max_date],
